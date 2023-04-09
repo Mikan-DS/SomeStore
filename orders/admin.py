e4114235355c30343b3abbb8ab3f222fe5bebc0d
72257910+Mikan-DS@users.noreply.github.com <72257910+Mikan-DS@users.noreply.github.com>
2023-04-10 01:24:29 +0300

    23041001

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.utils import timezone
@@ -56,24 +56,35 @@ class RejectOrdersForm(forms.ModelForm):
 
 
 class ReviewedOrderForm(forms.ModelForm):
+
     class Meta:
         model = ReviewedOrder
-        fields = ['order', 'admin_user', 'decision_date', 'rejection_reason']
+        fields = ['order', 'decision_date', 'rejection_reason'] # 'admin_user',
         labels = {
             'order': "Заказ",
             'admin_user': 'Администратор',
             'decision_date': 'Дата решения',
             'decision': 'Решение',
             'rejection_reason': 'Причина отклонения',
+            'approver': 'Администратор',
+
         }
+
 
     APPROVE = 'approve'
     REJECT = 'reject'
 
     decision = forms.ChoiceField(choices=((APPROVE, 'Принять'), (REJECT, 'Отклонить')), widget=forms.RadioSelect, label='Решение', initial=APPROVE)
 
+    approver = forms.CharField(label="Администратор")
+
+
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        if self.instance.pk is not None:
+            self.fields['order'].widget = forms.HiddenInput()
 
         if self.instance.rejection_reason is None:
             self.fields['decision'].initial = self.APPROVE
@@ -81,6 +92,11 @@ class ReviewedOrderForm(forms.ModelForm):
             self.fields['decision'].initial = self.REJECT
 
         self.fields['rejection_reason'].initial = self.instance.rejection_reason
+
+        self.fields['approver'].initial = self.instance.admin_user
+
+        self.fields['decision_date'].widget.attrs['readonly'] = True #TODO по вкусу
+        self.fields['approver'].widget.attrs['readonly'] = True
 
     def clean(self):
         cleaned_data = super().clean()
@@ -91,6 +107,9 @@ class ReviewedOrderForm(forms.ModelForm):
         reviewed_order = super().save(commit=False)
         decision = self.cleaned_data.get('decision')
         rejection_reason = self.cleaned_data.get('rejection_reason')
+
+        reviewed_order.decision_date = timezone.now()
+        reviewed_order.decision_date = timezone.now()
 
         if decision == 'approve':
             reviewed_order.rejection_reason = None
@@ -109,6 +128,16 @@ class ReviewedOrderAdmin(admin.ModelAdmin):
     verbose_name = 'Рассмотренный заказ'
     verbose_name_plural = 'Рассмотренные заказы'
     actions = ['approve_orders', 'reject_orders']
+
+    fieldsets = (
+        ('Информация', {
+            'fields': ('order', 'approver', 'decision_date')
+        }),
+        ('Статус', {
+            'classes': ('wide',),
+            'fields': ('decision', 'rejection_reason')
+        }),
+    )
 
     def approve_orders(self, request, queryset):
         queryset.update(rejection_reason=None)
@@ -142,5 +171,18 @@ class ReviewedOrderAdmin(admin.ModelAdmin):
         return initial
 
     def get_form(self, request, obj=None, **kwargs):
-        return ReviewedOrderForm
+        form = ReviewedOrderForm
+        form.request = request
+        return form
 
+    def save_form(self, request, form, change):
+
+        form.instance.admin_user = request.user
+
+        return form.save(commit=False)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        obj = self.get_object(request, object_id)
+        if obj is not None and obj.decision_date is not None:
+            self.message_user(request, 'Чтобы отправить заказ обратно на рассмотрение удалите запись', level=messages.WARNING)
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
